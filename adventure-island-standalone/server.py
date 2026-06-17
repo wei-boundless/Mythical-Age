83ac083cbcf6999ad29c039039b9e357edee57bc
wei-boundless <1170748727@qq.com>
2026-06-18 00:40:55 +0800

    Clean repository upload surface

--- adventure-island-standalone/server.py
+++
@@ -1,7 +0,0 @@
-import http.server
-import os
-
-os.chdir(r"D:\AI应用\langchain-agent\adventure-island-standalone")
-server = http.server.HTTPServer(("127.0.0.1", 8088), http.server.SimpleHTTPRequestHandler)
-print("Serving adventure-island-standalone on http://127.0.0.1:8088")
-server.serve_forever()
